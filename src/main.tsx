dfbd6045b3d0e4843ad8ba6850c0640e808952e2
Rodolphe Renaux <rrenaux@idobs.net>
2025-12-10 17:38:13 +0100

    fixup! Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ import './app.scss'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <Provider store={store}>
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
               <Routes>
                   <Route element={<Layout />}>
                       <Route index element={<Home />} />
